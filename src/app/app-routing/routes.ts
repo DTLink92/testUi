38ac4b81cd5bbcc57ace9b38a4a51ff8bc845ff3
Edson Terceros <Edson.Terceros@agilysys.com>
2018-04-24 19:40:18 -0400

    uso de activated route y location

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -3,11 +3,13 @@ import {HomeComponent} from '../home/home.component';
 import {CatalogComponent} from '../catalog/catalog.component';
 import {ContactComponent} from '../contact/contact.component';
 import {AboutComponent} from '../about/about.component';
+import {ItemDetailComponent} from '../item-detail/item-detail.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'catalog', component: CatalogComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'itemdetail/:id', component: ItemDetailComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Item} from '../shared/item';
+import {ItemService} from '../services/item.service';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-item-detail',
@@ -7,13 +10,20 @@ import {Item} from '../shared/item';
   styleUrls: ['./item-detail.component.scss']
 })
 export class ItemDetailComponent implements OnInit {
-  @Input()
   item: Item;
 
-  constructor() {
+  constructor(private itemService: ItemService,
+              private route: ActivatedRoute,
+              private location: Location) {
   }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.item = this.itemService.getItem(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
