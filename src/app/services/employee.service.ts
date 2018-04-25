c6c043dbd58cca9520235a12b60384a99297d69c
Edson Terceros <Edson.Terceros@agilysys.com>
2018-04-24 20:44:36 -0400

    tarea 2: parte 1

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import {Employee} from '../shared/employee';
+import {EMPLOYEES} from '../shared/employees';
 
 @Injectable()
 export class EmployeeService {
@@ -6,4 +8,15 @@ export class EmployeeService {
   constructor() {
   }
 
+  getEmployees(): Employee[] {
+    return EMPLOYEES;
+  }
+
+  getEmployee(id: number): Employee {
+    return EMPLOYEES.filter((employee) => employee.id === id)[0];
+  }
+
+  getFeaturedEmployee(): Employee {
+    return EMPLOYEES.filter((employee) => employee.featured)[0];
+  }
 }
