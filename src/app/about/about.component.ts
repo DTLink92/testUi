5e5811d1c20ca5bce8ffeda83146bdbc00794345
Edson Terceros <Edson.Terceros@agilysys.com>
2018-04-24 20:58:51 -0400

    tarea 2: parte 2 y 3

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Employee} from '../shared/employee';
+import {EmployeeService} from '../services/employee.service';
 
 @Component({
   selector: 'app-about',
@@ -7,10 +9,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() {
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) {
   }
 
   ngOnInit() {
+    this.employees = this.employeeService.getEmployees();
   }
 
 }
