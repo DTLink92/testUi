79404456575d5ba7b8de64f3824160ae2cdad4e0
bpo92007@gmail.com <bpo92007@gmail.com>
2018-05-19 10:30:58 -0400

    Added method to post to contract service, added styles to employee-creation form

--- src/app/services/contractService/contract-service.service.ts
+++ src/app/services/contractService/contract-service.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Contract} from '../../shared/contract';
+import {Employee} from '../../shared/employee';
+import {EmployeeDetail} from '../../shared/employeeDetail';
 
 @Injectable()
 export class ContractService {
@@ -15,6 +17,31 @@ export class ContractService {
 
   postContract(contract: Contract): Observable<Contract> {
     return this.http.post<any>('http://localhost:8080/position_assign_employees', contract, this.httpOptions)
+      .map((res) => {
+        return res;
+      }).catch(error => {
+        return error;
+      });
+  }
+
+  updateContract(contract: Contract): Observable<any> {
+    return this.http.put<any>('http://localhost:8080/position_assign_employees', contract)
+      .map((res) => {
+        return <Contract>res;
+      }).catch(error => {
+        return error;
+      });
+  }
+  getEmployeesByPosition(id: number): Observable<any> {
+    return this.http.get<any>('http://localhost:8080/position_assign_employees/position=' + id)
+      .map((res) => {
+        return res;
+      }).catch(error => {
+        return error;
+      });
+  }
+  deleteContract(id: number): Observable<any> {
+    return this.http.delete<any>('http://localhost:8080/position_assign_employees/' + id)
       .map((res) => {
         return res;
       }).catch(error => {
